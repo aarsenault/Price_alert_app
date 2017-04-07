90ea5401ebc07facbc476af374c40f65169d90fa
Adriel Arsenault <adriel.arsenault@gmail.com>
2017-04-07 14:38:05 -0400

    changed templates to Jinja2

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from src.common.database import Database
 
@@ -14,6 +14,13 @@ app.secret_key = '123'
 @app.before_first_request
 def init_db():
     Database.initialize()
+
+
+@app.route('/')
+def home():
+    return render_template('home.jinja2')
+
+
 
 from src.models.users.views import user_blueprint
 app.register_blueprint(user_blueprint, url_prefix="/users")
